7b1b79e06d2caf72043d9f9d059b88929d851df6
Dmitriy Babkov <dsbabkov@gmail.com>
2017-03-03 08:25:55 +0300

    Добавлена вьюха дерева с прикрученной к ней моделью

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,11 +1,13 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
+#include "TreeModel.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->treeView->setModel(new TreeModel(this));
 }
 
 MainWindow::~MainWindow()
